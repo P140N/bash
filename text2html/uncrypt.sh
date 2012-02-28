886e51e690a72d007fc368aa36733fd0452ff622
P140N <P140N@yandex.ru>
2012-02-28 04:33:47 +0300

    sad

--- text2html/uncrypt.sh
+++ text2html/uncrypt.sh
@@ -1,54 +1,33 @@
 #!/bin/bash
 
-pass=0
-m=0
-n=0
-j=0
 i=0
-success=0
+rdy=0
+pass=0000
 
-while [ m -le 9 ]
+
+while [ $rdy -eq 0 ]
 do
-		while [ n -le 9 ]
+    echo "$pass"
+    rm -f tex2html-test.tar
+    if 7z x -p$pass tex2html-test.tar.7z >/dev/null 2>&1
+    then
+        echo "pass $pass is correct"
+        rdy=1
+    else
+        i=$[i + 1]
+        pass=$i
+		while [ ${#pass} -ne 4 ]
 		do
-				while [ j -le 9 ]
-				do
-						while [ i -le 9 ]
-						do
-							pass="$m$n$j$i"
-							echo "try: $pass"
-							rm -f tex2html-test.tar
-							if 7z x -p$pass tex2html-test.tar.7z >/dev/null 2>&1
-							then
-								echo "pass $pass is correct"
-								success=1															
-							fi
-							if [success -ne 1]&&[pass -eq 9999]
-								echo "pass not decrypted"
-								exit 1
-							fi
-							if [success -eq 1]
-								break  
-							fi
-							i=$[i + 1]
-						done
-				if [success -eq 1]
-					break  
-				fi
-				j=$[j + 1]
-				done
-		if [success -eq 1]
-			break  
-		fi
-		n=$[n + 1]
+			pass="0$pass"
 		done
-	if [success -eq 1]
-		break  
-	fi
-	m=$[m + 1]
+        if [ $i -gt 9999 ]
+        then
+            echo "fail"
+            exit 1
+        fi
+    fi
 done
 
-echo "extracting"
 tar -xf tex2html-test.tar
 
 
